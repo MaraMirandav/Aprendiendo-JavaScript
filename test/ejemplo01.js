8dcdea098d113350747c185d67bd10d4459bba10
María Elena Miranda <mara.mirandav@gmail.com>
2025-08-05 10:54:59 +0200

    Modificación de nombres de las carpetas, para visualización ordenada

--- test/ejemplo01.js
+++
@@ -1,9 +0,0 @@
-export class Persona {
-    constructor(nombre, apellido) {
-        this._nombre = nombre;
-        this._apellido = apellido;
-    }
-    toString() {
-        return `${this._nombre} ${this._apellido}`
-    }
-}
